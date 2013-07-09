67149dab565056f023a27514b9e16b9c3340fda9
gyulusboy <gyulusboy@localhost>
2013-07-09 09:49:38 +0200

    Initial Commit Project

---
+++ IIOEngine/Marker.cs
@@ -0,0 +1,9 @@
+namespace IntelliFactory.WebSharper.IIOEngine.Resources
+{
+    using IntelliFactory.WebSharper;
+
+    public class IIOEngine : Core.Resources.BaseResource
+    {
+        public IIOEngine() : base("iioEngine-1.2.2.min.js") { }
+    }
+}
