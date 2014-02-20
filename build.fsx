9e2179a4cb7e8de3c9ec30caa93d2b582e855628
jankoa <jankoa@localhost>
2014-02-20 15:26:48 +0100

    correct PackageId

--- build.fsx
+++ build.fsx
@@ -2,7 +2,7 @@
 open IntelliFactory.Build
 
 let bt =
-    let bt = BuildTool().PackageId("WebSharper.D3", "2.5").References(fun r -> [r.Assembly "System.Web"])
+    let bt = BuildTool().PackageId("WebSharper.iioEngine", "2.5").References(fun r -> [r.Assembly "System.Web"])
     bt.WithFramework(bt.Framework.Net40)
 
 let main =
