1e9269515effd3f141aa132af18aa1d463662bea
Loic Denuziere <loic@denuziere.net>
2018-01-23 14:34:16 +0100

    Convert to .NET Standard

--- build.fsx
+++ build.fsx
@@ -1,34 +1,17 @@
-#load "tools/includes.fsx"
-open IntelliFactory.Build
+#load "paket-files/build/intellifactory/websharper/tools/WebSharper.Fake.fsx"
+open Fake
+open WebSharper.Fake
 
-let bt =
-    BuildTool().PackageId("WebSharper.iioEngine")
-        .VersionFrom("WebSharper", versionSpec = "(,4.0)")
-        .WithFSharpVersion(FSharpVersion.FSharp30)
-        .WithFramework(fun fw -> fw.Net40)
-        .References(fun r -> [r.Assembly "System.Web"])
+let targets =
+    GetSemVerOf "WebSharper"
+    |> ComputeVersion
+    |> WSTargets.Default
+    |> MakeTargets
 
-let main =
-    bt.WebSharper.Extension("WebSharper.iioEngine")
-        .SourcesFromProject()
-        .Embed(["iioEngine-1.2.2.min.js"; "Box2dWeb-2.1.a.3.min.js"])
+Target "Build" DoNothing
+targets.BuildDebug ==> "Build"
 
-bt.Solution [
-    main
+Target "CI-Release" DoNothing
+targets.CommitPublish ==> "CI-Release"
 
-    bt.NuGet.CreatePackage()
-        .Description("WebSharper bindings to iio Engine (http://iioengine.com/) version 1.2.2")
-        .ProjectUrl("http://github.com/intellifactory/websharper.iioengine")
-        .Configure(fun c ->
-            {
-                c with
-                    Authors = ["IntelliFactory"]
-                    Id = "WebSharper.iioEngine"
-//                    LicenseUrl = Some "http://github.com/intellifactory/websharper.iioengine/blob/master/LICENSE.md"
-//                    RequiresLicenseAcceptance = true
-                    Title = Some "WebSharper.iioEngine (1.2.2)"
-            })
-        .Add(main)
-
-]
-|> bt.Dispatch
+RunTargetOrDefault "Build"
